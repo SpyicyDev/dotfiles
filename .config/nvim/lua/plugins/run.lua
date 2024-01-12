f8b822e9bdfeb1f418acaaf6e70c36c8a4c607c2
Mack Haymond <mack.haymond@icloud.com>
2024-01-11 21:16:21 -0500

    added nvim config

---
+++ .config/nvim/lua/plugins/run.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "spyicydev/run.nvim",
+        dependencies = {
+            "numToStr/FTerm.nvim",
+        },
+        opts = {
+            filetype = {
+                python = function()
+                    if vim.fn.findfile("pyproject.toml", ".;") ~= "" then
+                        return "poetry run python3 %f"
+                    else
+                        return "python3 %f"
+                    end
+                end,
+                rust = "cargo run",
+                lua = "lua %f",
+                markdown = ":MarkdownPreview"
+            },
+        },
+    }
+}
